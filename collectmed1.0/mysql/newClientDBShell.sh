01206d44911a3b0bd476d9322cff42be936885ab
Guillermo Cruz <gcruz@collectmed.net>
2012-06-23 14:52:46 -0500

    1

--- collectmed1.0/mysql/newClientDBShell.sh
+++ collectmed1.0/mysql/newClientDBShell.sh
@@ -1,13 +1,13 @@
 
 
-if [ test $9 -eq 1 ]
+if $9 -eq 1 
 then
 
 	# REM CREATE THE NEW DATABASE
 	$1 --host=$2 --port=$3 --user=$4 --password=$5 < $7
 
 
-elif [ test $9 -eq 2 ] 
+elif $9 -eq 2  
 then
 
 	# REM APPLY A SCHEMA TO THE NEW DATABASE TO BUILD ITS TABLES.
