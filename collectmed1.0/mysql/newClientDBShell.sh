95e299966c5dc0cbf15a2ec3ffe68280ccdd323b
Guillermo Cruz <gcruz@collectmed.net>
2012-06-26 04:42:49 -0500

    1

--- collectmed1.0/mysql/newClientDBShell.sh
+++ collectmed1.0/mysql/newClientDBShell.sh
@@ -1,14 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
+arg="$9"
 
-if [ "$9" = "1" ] 
-then 
-    #REM CREATE THE NEW DATABASE
-	$1 --host=$2 --port=$3 --user=$4 --password=$5 < $7
-elif [ "$9" = "2" ] 
-then 
-	#REM APPLY A SCHEMA TO THE NEW DATABASE TO BUILD ITS TABLES.
-	$1 --host=$2 --port=$3 --user=$4 --password=$5 $6 < $8  
-else 
-    echo "never heard of $9" 
-fi 
+usage(){
+    echo "Usage: $0 1|2"
+}
 
+case $arg in
+    1) $1 --host=$2 --port=$3 --user=$4 --password=$5 < $7;;
+    2) $1 --host=$2 --port=$3 --user=$4 --password=$5 $6 < $8;;
+    *) usage
+esac
