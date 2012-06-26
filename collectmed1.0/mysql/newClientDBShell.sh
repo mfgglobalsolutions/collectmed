13be55df035cb78b018b86dfca1e2d150090ce85
Guillermo Cruz <gcruz@collectmed.net>
2012-06-26 06:13:03 -0500

    1

--- collectmed1.0/mysql/newClientDBShell.sh
+++ collectmed1.0/mysql/newClientDBShell.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 arg="$9"
 
-usage(){
-    echo "Usage: $0 1|2"
-}
+    echo "Usage: $0, $1, $2"
 
-case $arg in
-    1) $1 --host=$2 --port=$3 --user=$4 --password=$5 < $7;;
-    2) $1 --host=$2 --port=$3 --user=$4 --password=$5 $6 < $8;;
-    *) usage
-esac
