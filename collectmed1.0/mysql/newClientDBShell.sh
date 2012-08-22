da9fd8a9171e2e18be17f756620d8fca61b80f77
Guillermo Cruz <gcruz@collectmed.net>
2012-08-22 16:58:45 +0200

    1

--- collectmed1.0/mysql/newClientDBShell.sh
+++ collectmed1.0/mysql/newClientDBShell.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mysql pa_master<<EOFMYSQL
-UPDATE client SET ClientName = 'MFG Global Medical Inc.' WHERE ClientID = 1084;
-EOFMYSQL
+result=$(mysql --host=$2 --port=$3 --user=$4 --password=$5 $6 -sN -e “UPDATE client SET ClientName = 'MFG Global Medical Inc.' WHERE ClientID = 1084”)
