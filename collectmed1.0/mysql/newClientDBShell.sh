384f61ec79d7bc621fa15cd630ae436b883e634d
Guillermo Cruz <gcruz@collectmed.net>
2012-08-22 20:01:12 +0200

    1

--- collectmed1.0/mysql/newClientDBShell.sh
+++ collectmed1.0/mysql/newClientDBShell.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-mysql --host=127.0.0.1 --port=3306 --user=root --password=collectmednet --database=paclient_1099 < /usr/share/apache-tomcat-7.0.27/webapps/collectmed/collectmed1.0/mysql/paclient_master.sql"
-						
+mysql --host=127.0.0.1 --port=3306 --user=root --password=collectmednet --database=paclient_1099 < /usr/share/apache-tomcat-7.0.27/webapps/collectmed/collectmed1.0/mysql/paclient_master.sql
